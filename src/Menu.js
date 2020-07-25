a0c7b6336cdfd095691c026836fe83ee71d894cb
Brendon Dalaba <46267340+bdalaba@users.noreply.github.com>
2020-07-25 18:17:01 -0400

    added more training links in menu

--- src/Menu.js
+++ src/Menu.js
@@ -32,6 +32,24 @@ export default class Menu extends React.Component {
             <a href="https://docs.google.com/presentation/d/12f4gkiVWMljvXcK2eVzMSqWrWv2bEToN8MmxXOceRfM/present?slide=id.p">
               <p>BossLM Mobile</p>
             </a>
+            <a href="https://docs.google.com/presentation/d/1r3rp5Caiij09YXaAq4jBa8On8niPvUEI3v6f9AGlIIg/edit">
+              <p>Lawn Maintenance</p>
+            </a>
+            <a href="https://docs.google.com/presentation/d/1IFI5iq8yTsbG5VFspRc-jmEOn-AAACddLqw9l5qbiYA/present?slide=id.g38182816e_05">
+              <p>Sidewalk Shoveling and Ice Melt</p>
+            </a>
+            <a href="https://docs.google.com/presentation/d/1BzOMRuicd_vhlQM4q89bn1HWzikplvyW9_wuUoCq2lg/present#slide=id.p">
+              <p>Truck Ice Melt</p>
+            </a>
+            <a href="https://docs.google.com/presentation/d/10Vh1w4PnDSO6Fn-xl1eO8fuFtURaTYYcWFawMLkSgzs/present?slide=id.g34b5061cf_00">
+              <p>Plow Operator</p>
+            </a>
+            <a href="https://docs.google.com/presentation/d/1tEwx5GWrnGyX6qilsau3hk7Vodua_pXyCIQ2mKvfPbc/present#slide=id.p">
+              <p>Bobcat/Loader Operator</p>
+            </a>
+            <a href="https://docs.google.com/presentation/d/1c6fX_XIYsQHwdyeL4ZO7kgQS2r8YD3YM8PD8Fk8KuKk/present#slide=id.p">
+              <p>Quad/RTV Operator</p>
+            </a>
           </li>
           <li>
             <h2>Settings</h2>
